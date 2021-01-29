e2c08a4713bd5e3e387a09d82323d5efa46b1887
Gokhan Ozdemir <77547072+gokhannozzdemir@users.noreply.github.com>
2021-01-29 21:43:58 +0300

    Add files via upload

---
+++ GamerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class GamerManager : IGamerService
+
+       IUserValidationService _userValidationService;
+
+    {
+        private object _userValidationService;
+
+        public GamerManager(IUserValidationService userValidationService)
+        {
+            _userValidationService = userValidationService;
+        }
+        public void Add(Gamer gamer)
+        {
+            if (_userValidationService.Validate(gamer)==true)
+            {
+                Console.WriteLine("Kayit oldu");
+            }
+            else
+            {
+                Console.WriteLine("Dogrulama basarisiz. Kayit basarisiz");
+
+            }
+        }
+
+     
+        }
+    }
+}
---
+++ UserValidationManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    { 
+        public bool Validate(Gamer gamer)
+        {
+            if(gamer.BirthYear==1985 && gamer.FirstName=="GOKHAN"
+                &&gamer.LastName =="OZDEMIR" && gamer.IdentityNumber==1111)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+}
